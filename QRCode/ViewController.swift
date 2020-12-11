e880f67aa5fcfd02d6d0a9f27f74d7f91b84c49c
Yonatan Tovim <tovimyonatan@gmail.com>
2020-12-11 19:37:04 +0200

    Initial Commit

---
+++ QRCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCode
+//
+//  Created by Yonatan Tovim on 11/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
